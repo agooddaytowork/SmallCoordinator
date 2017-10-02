329fc9eec397668f7ff68be6336bfda3db637dda
anphan2410 <an.phan@ascenx.com>
2017-10-02 17:34:12 +0700

    saved work

--- mainwindow.h
+++ mainwindow.h
@@ -14,6 +14,9 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_pushButtonQuitApplication_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
