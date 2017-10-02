c4fa698709ece4c3eb8eb3073479f831ce1ce789
anphan2410 <an.phan@ascenx.com>
2017-10-02 17:33:47 +0700

    saved work di an oc

--- commonthings.cpp
+++ commonthings.cpp
@@ -6,21 +6,4 @@ const QString UHV4WorkerObjName = QStringLiteral("UHV4Worker");
 const QString UHV2PVICollectorObjName = QStringLiteral("UHV2PVICollector");
 const QString UHV4PVICollectorObjName = QStringLiteral("UHV4PVICollector");
 const QString SmallCoordinatorObjName = QStringLiteral("SmallCoordinator");
-const QSqlDatabase &localQSqlDatabase = [](){
-    QSqlDatabase lambdaReturn = QSqlDatabase::addDatabase("QMYSQL","originalLocalQSqlDatabaseConnection");
-    lambdaReturn.setHostName("localhost");
-    lambdaReturn.setDatabaseName("raspberry");
-    lambdaReturn.setUserName("root");
-    lambdaReturn.setPassword("Ascenx123");
-    lambdaReturn.setPort(3306);
-    if (lambdaReturn.open())
-    {
-        anAck("Local Database Connected !");
-    }
-    else
-    {
-        anError("Failed To Connect Local Database !");
-        exit(EXIT_FAILURE);
-    }
-    return lambdaReturn;
-}();
+
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,8 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButtonQuitApplication_clicked()
+{
+    qApp->quit();
+}
